ca5611431d0362470d1d588fa8e99286eba11349
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-01-20 22:42:22 +0000

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -50,6 +50,10 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
 		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the chosen piece");
+			
+		}
 	}
 	
 	
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -15,5 +15,11 @@ public class King extends ChessPiece{
 	public String toString() {
 		return "K";
 	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean [getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
 	
 }
