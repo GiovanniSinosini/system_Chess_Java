f48815b56573ade5f2b70530ca30944c45a6130a
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-01-23 18:26:20 +0000

    Pawn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,6 +8,7 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
+import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -219,12 +220,30 @@ public class ChessMatch {
 	
 	
 	private void initialSetup() {
-		placeNewPiece('h', 7, new Rook(board, Color.WRITE));
-        placeNewPiece('d', 1, new Rook(board, Color.WRITE));
+		placeNewPiece('a', 1, new Rook(board, Color.WRITE));
         placeNewPiece('e', 1, new King(board, Color.WRITE));
-
-        placeNewPiece('b', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('a', 8, new King(board, Color.BLACK));
+        placeNewPiece('h', 1, new Rook(board, Color.WRITE));
+        placeNewPiece('a', 2, new Pawn(board, Color.WRITE));
+        placeNewPiece('b', 2, new Pawn(board, Color.WRITE));
+        placeNewPiece('c', 2, new Pawn(board, Color.WRITE));
+        placeNewPiece('d', 2, new Pawn(board, Color.WRITE));
+        placeNewPiece('e', 2, new Pawn(board, Color.WRITE));
+        placeNewPiece('f', 2, new Pawn(board, Color.WRITE));
+        placeNewPiece('g', 2, new Pawn(board, Color.WRITE));
+        placeNewPiece('h', 2, new Pawn(board, Color.WRITE));
+        
+        
+        placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('h', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('c', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('d', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('e', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('f', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('g', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('h', 7, new Pawn(board, Color.BLACK));
 	}
 	
 	
